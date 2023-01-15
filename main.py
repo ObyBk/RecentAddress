fecc2a6188f342bd6eb2f22b668f2473bde5c8e5
Oby Bkkourh <93168690+ObyBk@users.noreply.github.com>
2023-01-15 13:05:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Name: Oby Bkkourh
+# Project's name: Find recent addresses
+
+
+# This function takes a list of tuples, where each tuple contains an address
+# string and a date string in this format: "L3, 20 queen road,1992" from the user input
+# which is in the following while loop
 
 def most_recent_date(addresses_and_dates):
     most_recent_dates = {}
@@ -11,6 +18,10 @@ def most_recent_date(addresses_and_dates):
     return most_recent_dates
 
 addresses_and_dates = []
+
+# This loop is meant to take user input and then put it in a tuple and then separate
+# commas to find the required values
+# or if user input is "done" show results
 while True:
     input_str = input("Enter an address and date in the format 'address,YYYY-MM-DD' or 'done' to finish: ")
     if input_str.strip().lower() == 'done':
